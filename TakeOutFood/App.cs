525f821b12b3f56f722ba6af7634af78eb636c68
Shishi Zhang <SZhang42@slb.com>
2020-11-03 20:09:46 +0800

    Add best price

--- TakeOutFood/App.cs
+++ TakeOutFood/App.cs
@@ -2,6 +2,8 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
 
     public class App
     {
@@ -16,9 +18,108 @@
 
         public string BestCharge(List<string> inputs)
         {
-            //TODO: write code here
+            var avaliableItem = itemRepository.FindAll();
+            var avaliablePromotion = salesPromotionRepository.FindAll();
+            var itemInShoppingCart = new List<(Item item, double itemPrice)>();
+            //Dictionary<string, (Item item, int quantity)> shoppingCart = new Dictionary<string, (Item item, int quantity)>();
+            var receipt = "============= Order details =============\n";
+            double totalPrice = 0;
 
-            return null;
+            foreach (var itemString in inputs)
+            {
+                (string id, int quantity) itemInfo = Deserialize(itemString);
+                var item = avaliableItem.Where(i => i.Id == itemInfo.id).FirstOrDefault();
+                var itemPrice = item.Price * itemInfo.quantity;
+                receipt = receipt + item.Name + " x " + itemInfo.quantity + " = " + itemPrice + " yuan\n";
+                itemInShoppingCart.Add((item, itemPrice));
+                totalPrice += itemPrice;
+            }
+
+            var validPromotion = GetValidPromotion(itemInShoppingCart, avaliablePromotion);
+
+           
+            if (validPromotion.Count != 0)
+            {
+                receipt = receipt + "-----------------------------------\n";
+                var bestDiscount = GetBestDiscount(itemInShoppingCart, avaliablePromotion);
+                receipt = receipt +
+                    "Promotion used:\n" +
+                    $"Half price for certain dishes ({bestDiscount.discountItems}), saving {bestDiscount.bestDiscount} yuan\n";
+                totalPrice = totalPrice - bestDiscount.bestDiscount;
+            }
+
+            receipt = receipt +
+                    "-----------------------------------\n" +
+                    $"Total：{totalPrice} yuan\n" +
+                    "===================================";
+
+            return receipt;
+        }
+        //to do: add validation 
+        private (string id, int quantity) Deserialize(string input)
+        {
+            var itemInfo = input.Split(" x ");
+            int quantity = int.Parse(itemInfo[1]);
+            return (itemInfo[0], quantity);
+        }
+
+        private List<SalesPromotion> GetValidPromotion(List<(Item item, double price)> items, List<SalesPromotion> promotions)
+        {
+            List<SalesPromotion> itemWithPromotion = new List<SalesPromotion>();
+
+            foreach (var promotion in promotions)
+            {
+                bool existsCheck = promotion.RelatedItems.All(x => items.Any(y => x == y.item.Id));
+                if (existsCheck)
+                {
+                    itemWithPromotion.Add(promotion);
+                }
+            }
+            return itemWithPromotion;
+        }
+
+        private (double bestDiscount, string discountItems) GetBestDiscount(List<(Item item, double price)> items, List<SalesPromotion> validPromotions)
+        {
+            double bestDiscount = 0;
+            var bestDiscountItems = "";
+            foreach (var promotion in validPromotions)
+            {
+                double discountPrice = 0;
+                var discountItems = "";
+                foreach (var id in promotion.RelatedItems)
+                {
+                    var discountItem = items.Where(i => i.item.Id == id);
+                    
+                    if (discountItem.Any())
+                    {
+                        discountItems = discountItems + $" {discountItem.FirstOrDefault().item.Name},";
+                        discountPrice += discountItem.FirstOrDefault().price / 2;
+                    }
+                }
+                if (discountPrice > bestDiscount)
+                {
+                    bestDiscount = discountPrice;
+                    bestDiscountItems = discountItems;
+                }
+            }
+
+            return (bestDiscount, bestDiscountItems.Substring(1, bestDiscountItems.Length - 2));
+        }
+    }
+
+    public class ItemRepository : IItemRepository
+    {
+        public List<Item> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class SalesPromotionRepository : ISalesPromotionRepository
+    {
+        List<SalesPromotion> ISalesPromotionRepository.FindAll()
+        {
+            throw new NotImplementedException();
         }
     }
 }
